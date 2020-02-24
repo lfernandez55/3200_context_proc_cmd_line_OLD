b8d2e11a6d051e1668f5922432c9d0770aa1571d
lfernandez55 <lfernandez@weber.edu>
2020-02-23 21:42:45 -0700

    Commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import sys
+from flask import Flask, render_template, url_for, Markup
+
+
+app = Flask(__name__)
+
+#see: https://junxiandoc.readthedocs.io/en/latest/docs/flask/flask_template.html
+@app.context_processor
+def utility_processor():
+    def render(view,permitted_roles,linkname):
+        if app.config.get('my_role') in permitted_roles:
+            link = "<a "  + " href='" + url_for(view) + "'>" + linkname + "</a>"
+            return Markup(link)
+        else:
+            return "A link would appear here but it requires " + str(permitted_roles) \
+                + " and the current logged in user is a " + app.config.get('my_role')
+    return dict(render=render)
+
+
+
+@app.route('/')
+def home():
+    # print (app.config.get('my_role'))
+    return render_template('index.html')
+
+@app.route('/course_page')
+def course_page():
+    return render_template('course_page.html')
+
+@app.route('/admin_page')
+def admin_page():
+    return render_template('admin_page.html')
+
+if __name__ == '__main__':
+    app.config['my_role'] = sys.argv[1]
+    app.run()
